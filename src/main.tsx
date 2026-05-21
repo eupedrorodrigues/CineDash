6dd5ce959ed32828ebc0cda783552b450c900c7f
eupedrorodrigues <pedrorsrodrigues1203@gmail.com>
2026-05-21 16:21:28 -0300

    refactor: remove React Query Devtools from application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 import { queryClient } from "./lib";
 import "./index.css";
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
       <Toaster richColors position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>,
